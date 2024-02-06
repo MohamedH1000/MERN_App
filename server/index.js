a52cedcb470b1a10a169f056dd7dbbf29f5cb254
MohamedH1000 <122827442+MohamedH1000@users.noreply.github.com>
2024-02-07 02:54:49 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit:'30mb', extended: true }))
 app.use(cors())
 app.use('/posts', postRoutes);
-const CONNECTION_URL = 'mongodb+srv://MERN_App:CtgYKBNTHiKs6gpl@cluster0.ovnqwgj.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL = ''
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL)
